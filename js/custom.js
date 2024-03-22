35f43e4fe62614ef91c28e81847df0b396a8d729
sanaafzal-cmd <105205204+sanaafzal-cmd@users.noreply.github.com>
2024-03-22 21:42:01 +0500

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
+
 (function($) {
     'use strict';
 
     // Main Navigation
-    $('.hamburger-menu').on('click', function() {
+    $( '.hamburger-menu' ).on( 'click', function() {
         $(this).toggleClass('open');
         $('.site-navigation').toggleClass('show');
     });
